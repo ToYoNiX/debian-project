1dedd53b3f288ac0d2978a01263381dd7169f721
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-05-09 23:55:36 +0300

    Create installBumblebeeStatus_withNeededFonts.sh

---
+++ scripts/installBumblebeeStatus_withNeededFonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install bumblebee-status and pango font
+sudo apt install bumblebee-status fonts-noto-color-emoji -y
+
+echo "Bumblebee-status and pango font installed successfully."
