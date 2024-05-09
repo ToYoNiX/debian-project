288753c0902a3d537b5c56e78b7b52ecab45aed4
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-10 01:33:03 +0300

    script update.

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -4,6 +4,9 @@
 # Install i3
 sudo apt install i3 -y
 
+mkdir -p ~/.config/i3/
+cp -r ../dotfiles/.config/i3/* ~/.config/i3/
+
 # Install bumblebee-status and pango font
 sudo apt install bumblebee-status fonts-noto-color-emoji -y
 echo "i3 with Bumblebee-status and pango font are installed successfully."
