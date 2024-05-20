42116cd61bd94e03a67f9f7935de9369018604cc
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-20 17:56:11 +0300

    updated scripts.

--- scripts/ufw.sh
+++ scripts/ufw.sh
@@ -6,6 +6,8 @@ sudo apt install ufw
 sudo ufw limit 22/tcp
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw enable
