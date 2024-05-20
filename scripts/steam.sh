ad233dd1ba5276ab55b38130dbdae331ce69b665
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-20 16:41:06 +0300

    add steam installer script.

---
+++ scripts/steam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install steam-installer steam-devices mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 nvidia-driver-libs:i386 -y
