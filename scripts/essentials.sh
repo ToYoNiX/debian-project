e73894c49c74beb21f0b12fc425c9d6993e5e89a
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-20 05:41:22 +0300

    updated scripts.

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-*
-sudo apt autoremove
+sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* -y
+sudo apt autoremove -y
 
 sudo apt install vim obs-studio uget vlc papirus-icon-theme bibata-cursor-theme kdeconnect -y
