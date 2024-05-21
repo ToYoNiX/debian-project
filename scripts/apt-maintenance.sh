f0fbc55c45ed6b9004b377cf546151843591a254
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-21 17:56:30 +0300

    updated scripts.

--- scripts/apt-maintenance.sh
+++ scripts/apt-maintenance.sh
@@ -4,4 +4,4 @@
 sudo rm -rfv /var/lib/apt/lists/*
 sudo apt-get clean
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -4,4 +4,4 @@
 sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor -y
 sudo apt autoremove -y
 
-sudo apt install vim obs-studio uget vlc papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
+sudo apt install vim gparted obs-studio uget vlc papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
