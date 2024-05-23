f45549db73e868e85cc53e8509c403fbd892f5b5
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-24 02:52:50 +0300

    updated scripts.

--- scripts/embedded.sh
+++ scripts/embedded.sh
@@ -6,3 +6,8 @@ sudo apt install arduino python3-serial ftdi-eeprom libcanberra-gtk-module libca
 # Add user to dialout group
 sudo usermod -aG dialout $USER
 
+# Fix window management issues
+sudo tee -a /etc/environment <<EOF
+_JAVA_AWT_WM_NONREPARENTING=1
+EOF
+
