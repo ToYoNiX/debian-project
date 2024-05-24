48d48f8f50e14c4b8922640d1cb5fa5792a24a97
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-05-24 04:24:52 +0300

    adding a new script.

---
+++ scripts/adw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+curl -s https://julianfairfax.gitlab.io/package-repo/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/julians-package-repo.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/julians-package-repo.gpg ] https://julianfairfax.gitlab.io/package-repo/debs packages main' | sudo tee /etc/apt/sources.list.d/julians-package-repo.list
+
+sudo apt update
+sudo apt install adw-gtk3 -y
+
+flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
