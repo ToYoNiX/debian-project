6e415061a5d84c04fe42e7528931a0191664d219
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-09 19:12:23 +0300

    Update mysql-install.sh

--- scripts/mysql-install.sh
+++ scripts/mysql-install.sh
@@ -6,13 +6,9 @@ sudo apt update
 # Install libaio bec its needed
 sudo apt-get install libaio1
   
-# Add the official MySQL repository for Debian 12 (replace 'mysql-apt-config_0.8.26-1_all.deb' with the latest version if needed)
-curl -sSL https://dev.mysql.com/get/mysql-apt-config_0.8.26-1_all.deb | sudo apt install -
+sudo apt install mariadb-server mariadb-client -y
 
 sudo apt update
-
-# Install MySQL server package
-sudo apt install mysql-server
 
 # Secure MySQL installation (prompts for password)
 sudo mysql_secure_installation
