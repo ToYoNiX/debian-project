df49374a883822e2adf1068b99dcf1dc31b26cbf
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-17 05:32:48 +0300

    some mods.

--- scripts/adw.sh
+++ scripts/adw.sh
@@ -11,3 +11,6 @@ sudo apt install adw-gtk3 -y
 flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+flatpak install io.github.realmazharhussain.GdmSettings
+flatpak install flathub com.github.GradienceTeam.Gradience
