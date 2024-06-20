98b151109de850d63cbd58337844eea3fec9c593
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-20 08:00:28 +0300

    updated scripts.

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -5,3 +5,6 @@ sudo apt install vim curl gparted obs-studio uget mpv yt-dlp papirus-icon-theme 
 
 sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc -y
 sudo apt autoremove -y
+
+flatpak install flathub io.missioncenter.MissionCenter
+flatpak install flathub com.github.scrivanolabs.scrivano
