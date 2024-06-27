36e5426a84dd8e35513c8be90acb592e01dcc591
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-27 13:46:08 +0300

    Create rmGnomeGames.sh

---
+++ scripts/rmGnomeGames.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt purge iagno lightsoff four-in-a-row gnome-robots pegsolitaire gnome-2048 hitori gnome-klotski gnome-mines gnome-mahjongg gnome-sudoku quadrapassel swell-foop gnome-tetravex gnome-taquin aisleriot gnome-chess five-or-more gnome-nibbles tali ; sudo apt autoremove
