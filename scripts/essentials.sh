ff4f7961b9cd17ce089c484ed0538deda1498071
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-28 08:10:17 +0300

    updated script.

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -3,7 +3,7 @@
 
 sudo apt install vim curl tree gparted obs-studio uget mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
 
-sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc -y
+sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer -y
 sudo apt autoremove -y
 
 flatpak install flathub io.missioncenter.MissionCenter
