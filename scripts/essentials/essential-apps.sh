3bf357b2d1b8feb5be4d815f7ef77e0732156f43
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-30 23:11:45 +0300

    updated script.

--- scripts/essentials/essential-apps.sh
+++ scripts/essentials/essential-apps.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
 
-sudo apt install vim curl tree gparted obs-studio mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
+sudo apt install vim curl tree uget gparted obs-studio mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
 
 sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer -y
 sudo apt autoremove -y
 
 flatpak install flathub io.missioncenter.MissionCenter
 flatpak install flathub com.github.scrivanolabs.scrivano
-flatpak install flathub org.freedownloadmanager.Manager
+
+wget https://raw.githubusercontent.com/ugetdm/uget-integrator/master/install/linux/install_uget_integrator.sh
+chmod +x install_uget_integrator.sh
+./install_uget_integrator.sh
+rm install_uget_integrator.sh
