efa6d1c41f69d64f9f330670ce2b0c7664940860
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-30 13:34:18 +0300

    updated script.

--- scripts/apps/steam.sh
+++ scripts/apps/steam.sh
@@ -3,4 +3,4 @@
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install steam-installer steam-devices mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 nvidia-driver-libs:i386 protontricks mangohud goverlay vkbasalt -y
+sudo apt install steam-installer steam-devices mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 nvidia-driver-libs:i386 protontricks mangohud goverlay vkbasalt gamemode -y
