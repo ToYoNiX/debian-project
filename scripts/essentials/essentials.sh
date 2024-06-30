6644d649655ea3e4e7d9ff96d719bc347845cb3f
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-30 18:37:27 +0300

    updated script,

--- scripts/essentials/essentials.sh
+++ scripts/essentials/essentials.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 
-sudo apt install vim curl tree gparted obs-studio persepolis mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
+sudo apt install vim curl tree gparted obs-studio mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
 
 sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer -y
 sudo apt autoremove -y
 
 flatpak install flathub io.missioncenter.MissionCenter
 flatpak install flathub com.github.scrivanolabs.scrivano
+flatpak install flathub org.freedownloadmanager.Manager
