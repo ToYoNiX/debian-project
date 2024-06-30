860e7dcf931da40fa2c77f7e0958bfc79118e3ba
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-06-30 18:32:48 +0300

    updated script.

--- scripts/essentials/essentials.sh
+++ scripts/essentials/essentials.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-sudo apt install vim curl tree gparted obs-studio uget mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
+sudo apt install vim curl tree gparted obs-studio persepolis mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
 
 sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer -y
 sudo apt autoremove -y
