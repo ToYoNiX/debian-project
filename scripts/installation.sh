8deb53e3bf7744bd24f5d217f7c1a461d979379c
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-07-01 19:41:08 +0300

    Added Java

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -184,6 +184,14 @@ programming() {
       echo "You can install them later using 'pip install <library_name>' command."
       break
       ;;
+
+    "Java")
+      echo "** Installing Java... ***"
+      sudo apt install openjdk-17-jdk openjdk-17-jre -y
+      echo "*** Installed OpenJDK 17 succesfully ***"
+      break
+      ;;
+
     "None")
       echo "Skipping programming language installation."
       break
