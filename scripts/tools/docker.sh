54190fe2e2f1667d40767ba2195c50ce61983c6d
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-07-12 01:16:32 +0300

    updated script.

--- scripts/tools/docker.sh
+++ scripts/tools/docker.sh
@@ -17,3 +17,6 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
+# Install the Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
