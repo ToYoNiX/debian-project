e6c4ddba7ed2ec4a024c2518d44c73e6f4f33f3f
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-07-12 01:21:46 +0300

    updated script.

--- scripts/tools/docker.sh
+++ scripts/tools/docker.sh
@@ -20,3 +20,6 @@ sudo apt-get update
 
 # Install the Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add user to the docker group
+sudo usermod -aG docker $USER
