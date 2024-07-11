094dd864c5133ce89beb1596170260ccf6f8b6e9
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-07-11 23:25:06 +0300

    updated script.

--- scripts/essentials/essential-apps.sh
+++ scripts/essentials/essential-apps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-sudo apt install vim curl tree uget gparted obs-studio mpv yt-dlp handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
+sudo apt install vim curl wget tree uget gparted obs-studio mpv handbrake papirus-icon-theme bibata-cursor-theme kdeconnect gnome-shell-extension-gsconnect -y
 
-sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer -y
+sudo apt purge firefox-esr gnome-music gnome-video-effects totem* rhythmbox* shotwell* libreoffice-* gnome-system-monitor vlc pidgin* brasero* hexchat* gdebi* sound-juicer yt-dlp -y
 sudo apt autoremove -y
 
 flatpak install flathub io.missioncenter.MissionCenter
@@ -13,3 +13,6 @@ wget https://raw.githubusercontent.com/ugetdm/uget-integrator/master/install/lin
 chmod +x install_uget_integrator.sh
 ./install_uget_integrator.sh
 rm install_uget_integrator.sh
+
+sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/yt-dlp
+sudo chmod a+rx /usr/bin/yt-dlp  # Make executable
