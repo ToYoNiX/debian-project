4b4f70e08488c030f38e0aa6d4f1dcb7c5a10dd8
Abdullah Azmy <abdullahazmy2@gmail.com>
2024-07-13 08:22:04 +0300

    Update compilerInstall.sh

--- scripts/apps/compilerInstall.sh
+++ scripts/apps/compilerInstall.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-#
 
 compilerInstallation() {
     echo "Select compilers to install (separated by spaces, or '0' to skip): "
     echo "Options:
-    1. vscode 
-    2. clion 
-    3. pycharm 
-    4. None"
+    1. Visual Studio Code
+    2. CLion
+    3. PyCharm
+    0. None"
 
     read -r -a compilers
 
     for compiler_index in "${compilers[@]}"; do
         case $compiler_index in
         1)
-            # Install vscode
-            echo "** Installing Visual Studio Code...**"
-            flatpak install flathub com.visualstudio.code
+            # Install Visual Studio Code
+            echo "** Installing Visual Studio Code... **"
+            flatpak install -y flathub com.visualstudio.code
             echo "Visual Studio Code installed successfully."
             ;;
         2)
-            # Install clion
-            echo "** Installing CLion...**"
-            flatpak install flathub com.jetbrains.CLion
+            # Install CLion
+            echo "** Installing CLion... **"
+            flatpak install -y flathub com.jetbrains.CLion
             echo "CLion installed successfully."
             ;;
         3)
-            # Install pycharm
-            echo "** Installing PyCharm...**"
-            flatpak install flathub com.jetbrains.PyCharm-Community
+            # Install PyCharm
+            echo "** Installing PyCharm... **"
+            flatpak install -y flathub com.jetbrains.PyCharm-Community
             echo "PyCharm installed successfully."
-            ;;
-        4)
-            echo "Skipping compiler installation."
-            return
             ;;
         0)
             echo "Skipping compiler installation."
