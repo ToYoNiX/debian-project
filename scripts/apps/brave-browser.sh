c20bdc98696d2ca008dae504d8919db587e6c1c0
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-07-20 16:24:57 +0300

    updated scritps.

--- scripts/apps/brave-browser.sh
+++ scripts/apps/brave-browser.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 #
 
-echo "Installing Brave"
-sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+if command -v brave-browser &> /dev/null ; then
+    echo "Brave browser is already installed."
+    exit 0
+fi
+
+braveInstall () {
+    echo "Installing Brave"
+    sudo apt install curl -y
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser -y
+}
+
+while ! command -v brave-browser &> /dev/null ; do
+    braveInstall
+done
+
+if command -v brave-browser &> /dev/null ; then
+    echo "Brave browser is installed successfully."
+else
+    echo "Something went wrong. -> Brave Browser."
+    exit 1
+fi
--- scripts/apps/google-chrome.sh
+++ scripts/apps/google-chrome.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 #
 
-echo "Installing Chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-rm google-chrome-stable_current_amd64.deb
+if command -v google-chrome &> /dev/null ; then
+    echo "Google chrome is already installed."
+    exit 0
+fi
+
+chromeInstall () {
+    echo "Installing Chrome"
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
+    rm google-chrome-stable_current_amd64.deb
+}
+
+while ! command -v google-chrome &> /dev/null ; do
+    chromeInstall
+done
+
+if command -v google-chrome &> /dev/null ; then
+    echo "Google chrome is installed successfully."
+else
+    echo "Something went wrong. -> Brave Browser."
+    exit 1
+fi
