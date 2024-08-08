44b822df895fafbee881b15b8e90ffa15cddb75b
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-08-08 06:59:39 +0300

    Update obsidian.sh

--- scripts/apps/obsidian.sh
+++ scripts/apps/obsidian.sh
@@ -4,6 +4,7 @@
 echo "Installing Obsidian..."
 URL=$(wget -q -O - https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep 'deb"$' | awk -F'"' ' {print $4} ')
 wget $URL -O obsidian.deb
+chmod +x obsidian.deb
 sudo apt install ./obsidian.deb
 rm obsidian.deb
 echo "Obsidian installed successfully."
