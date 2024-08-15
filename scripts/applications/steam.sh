4c48cafc71360c529d9d2bde80f4ff0d4f5b0f9c
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-08-16 01:53:14 +0300

    updated script.

--- scripts/applications/steam.sh
+++ scripts/applications/steam.sh
@@ -3,9 +3,10 @@
 
 echo "Adding 32-bit architecture support..."
 sudo dpkg --add-architecture i386
+sudo apt update
 
 # Install basic Steam and related packages
-sudo apt install steam-installer steam-devices mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 protontricks mangohud goverlay vkbasalt gamemode -y
+sudo apt install steam-installer steam-devices mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 protontricks gamemode -y
 
 # Check if NVIDIA drivers are installed
 if command -v nvidia-smi &> /dev/null ; then
