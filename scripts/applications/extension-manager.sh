863affc164d918ab662d3fb2c866a384772bb193
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-09-04 17:34:29 +0300

    Update extension-manager.sh

--- scripts/applications/extension-manager.sh
+++ scripts/applications/extension-manager.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
-
-# Install Gnome extension manager
-sudo apt install gnome-shell-extension-manager -y
+# Check if gnome-shell-extension-manager is already installed
+if dpkg -l | grep -q gnome-shell-extension-manager; then
+  echo "Gnome Shell Extension Manager is already installed."
+else
+  echo "Installing Gnome Shell Extension Manager..."
+  sudo apt install gnome-shell-extension-manager -y
+  if [[ $? -eq 0 ]]; then
+    echo "Gnome Shell Extension Manager installed successfully."
+  else
+    echo "Failed to install Gnome Shell Extension Manager."
+    exit 1
+  fi
+fi
